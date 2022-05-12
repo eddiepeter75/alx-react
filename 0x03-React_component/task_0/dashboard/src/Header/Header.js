d89395251be4aa0c70b6d678f61f7192f698ce7f
Eddie Peter <eddiepeter75@gmail.com>
2022-05-12 14:36:38 +0000

    React component

---
+++ 0x03-React_component/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+import logo from '../assets/logo.jpg';
+
+function Header() {
+        return (
+            <div className="App-header">
+          <img
+            src={logo}
+            alt="logo"
+          />
+          <h1>School dashboard</h1>
+        </div>
+        	);
+        }
+        
+        export default Header;
